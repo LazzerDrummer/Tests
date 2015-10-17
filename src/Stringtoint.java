1c486c8c6d3f308d4773fa774f2b1f2425b1f05f
mutantu2002 <mutantu2002@yahoo.com>
2015-10-17 12:24:18 +0300

    Add constant for byte code like the skrubs we are

--- src/Stringtoint.java
+++ src/Stringtoint.java
@@ -3,14 +3,17 @@ public class Stringtoint {
 
 	public static void main(String[] args) {
 		String c = "00345555555555556.45634566456456456456" ; 
+		byte commaCode=46;
+		byte zeroCode=48;
+		
 		double f=0,b=0,g=10;
 		int i,j=0;
 		byte[] chars=c.getBytes();
 		for (i = 0; i < chars.length; i++) {
-			if (chars[i]==46){
+			if (chars[i]==commaCode){
 				while (j<chars.length-i-1)
 				{
-					b=chars[j+i+1]-48;
+					b=chars[j+i+1]-zeroCode;
 					b=b/g;
 					f=f+b;
 					j++;g=g*10;
@@ -19,23 +22,23 @@ public class Stringtoint {
 				break;
 			}				
 			f=f*10;
-			f=f+chars[i]-48;	
+			f=f+chars[i]-zeroCode;	
 		}
 
 		f=0;b=0;
 	
 		for (i = 0; i < chars.length; i++) {
-			if (chars[i]==46){
+			if (chars[i]==commaCode){
 				break;
 			}				
 			f*=10;
-			f=f+chars[i]-48;	
+			f=f+chars[i]-zeroCode;	
 		}
 		b=f;
 		f=0;
 		for (j=chars.length-i-2;j>=0;j--)
 		{
-			f=f+chars[j+i+1]-48;
+			f=f+chars[j+i+1]-zeroCode;
 			f/=10.;
 		}
 		System.out.println("Stupid's code: "+(b+f));return;
