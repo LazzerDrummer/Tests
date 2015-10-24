7a40057bfaa4fc86f484553659237caecb166e41
Lazzer <lazerdrummer@gmail.com>
2015-10-24 11:52:57 +0300

    Added comments for easy use and minor structural edits.

--- src/FunkyGraph.java
+++ src/FunkyGraph.java
@@ -4,20 +4,31 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 class MyCanvas extends JComponent {
-  public void paint(Graphics g) {
-	double zoom=100;
-    g.drawLine(0,500,1000,500);
-    g.drawLine(500, 0, 500, 1000);
-
-    for(double x=-500/zoom;x<500/zoom;x=x+0.01/zoom)
-    {
-    	g.drawLine((int)(zoom*x+500),(int)(-zoom*f(x)+500),(int)(zoom*x+500),(int)(-zoom*f(x)+500));
-    }
+	private static final long serialVersionUID = 7684382000314660093L;
+	public void paint(Graphics g) {
+		//The variable zoom zooms the function. Also Increases the number of pixels added at x=x+0.01/zoom.
+		double zoom=100;
+	    g.drawLine(0,500,1000,500);
+	    g.drawLine(500, 0, 500, 1000);
+	
+	    for(double x=-500/zoom;x<500/zoom;x=x+0.01/zoom)
+	    {
+	    	g.drawLine((int)(zoom*x+500),(int)(-zoom*f(x)+500),(int)(zoom*x+500),(int)(-zoom*f(x)+500));
+	    }
     
-  }
-  public double f(double x){
-	  return Math.sin(10*x)+Math.pow(x, 2)+0.1;
-  }
+	}
+	/**
+	 * Put any functions you like
+	 * x+2
+	 * 2x-6
+	 * Math.pow(x,3)
+	 * Math.sin(x)
+	 * @param x
+	 * @return
+	 */
+	public double f(double x){
+		return Math.sin(10*x)+Math.pow(x, 2)+0.1;
+	}
 }
 
 public class FunkyGraph {
@@ -25,7 +36,8 @@ public class FunkyGraph {
 	    JFrame window = new JFrame();
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    window.setBounds(30, 30, 1000, 1000);
-	    window.getContentPane().add(new MyCanvas());
+	    MyCanvas myCanvas = new MyCanvas();
+		window.getContentPane().add(myCanvas);
 	    window.setVisible(true);
 
 	}
