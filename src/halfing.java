069c2ef9d86388ad7514542c6dae911032fb9f5d
Lazzer <lazerdrummer@gmail.com>
2015-11-26 20:20:48 +0200

    Added binary search for the number of digits
    and cut down the variable "car" and replaced with variable "j".
    In the binary search there is extra if (line 11) because most of the
    time i will be smaller than j by one, during the while.(ex i=2 j=3)and
    because i and j are both int (3+2/2 = 5/2 = 2) means the i wont be able
    to have the value 3,ever, so the while keeps running forever. But the
    extra else(line 12) does that, assigning i to j when i is smaller than j
    by one(line 12).

--- src/halfing.java
+++ src/halfing.java
@@ -3,22 +3,29 @@ import static java.lang.Math.*;
 public class halfing {
 
 	public static void main(String[] args) {
-		int n=42,a,b=0,i=1,car=1;
-		while(n/i>10)
+		int n=1255122,a,b=0,i=1,j=10;
+		while(i!=j)
 		{
-			car++;
-			i=i*10;
+			if((int)(n/pow(10,(i+j)/2))>0)
+			{
+				if (j-i!=1)i=(i+j)/2;
+				else i=j;
+			}
+			else
+			{
+				j=(i+j)/2;
+			}
 		}
-		System.out.println(car);
-		if(car%2!=0)
+		System.out.println(j);
+		if(j%2!=0)
 		{
-			a=(int) (n/pow(10,(car/2)+1));
-			b=(int) (n%pow(10,(car/2)));
+			a=(int) (n/pow(10,(j/2)+1));
+			b=(int) (n%pow(10,(j/2)));
 		}
 		else
 		{
-			a=(int) (n/pow(10,car/2));
-			b=(int) (n%pow(10,car/2));
+			a=(int) (n/pow(10,j/2));
+			b=(int) (n%pow(10,j/2));
 		}
 		System.out.println(a);
 		System.out.println(b);
