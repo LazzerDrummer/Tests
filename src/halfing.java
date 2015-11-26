80ee571d75bab123a176693abef19e29eed6b28d
Lazzer <lazerdrummer@gmail.com>
2015-11-27 00:49:23 +0200

    Added counts, a variable, and turned the extra if into a while condition

--- src/halfing.java
+++ src/halfing.java
@@ -3,19 +3,22 @@ import static java.lang.Math.*;
 public class halfing {
 
 	public static void main(String[] args) {
-		int n=1255122,a,b=0,i=1,j=10;
-		while(i!=j)
+		int n=12444,a,b=0,i=1,j=10;
+		int count=0;
+		while(j-i!=1)
 		{
-			if((int)(n/pow(10,(i+j)/2))>0)
+			count++;
+			int c = (int)(n/pow(10,(i+j)/2));
+			if(c>0)
 			{
-				if (j-i!=1)i=(i+j)/2;
-				else i=j;
+					i=(i+j)/2;
 			}
 			else
 			{
 				j=(i+j)/2;
 			}
 		}
+		System.out.println("count="+count);
 		System.out.println(j);
 		if(j%2!=0)
 		{
