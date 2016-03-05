dcf9f3a3935002ff49314908613518dc29b18498
Lazzer <lazerdrummer@gmail.com>
2016-03-05 12:00:48 +0200

    Greatly Optimized.

--- src/Pare.java
+++ src/Pare.java
@@ -3,19 +3,13 @@ import java.lang.*;
 public class Pare {
 
 	public static void main(String[] args) {
-		int i=1,d=0,p=0,a=2,b=300000,n,Par=0,Doi=0,aux;
+		int i=1,d=0,p=0,a=0,b=2000000000,n,Par=0,Doi=0,aux;
 		if(a%2==1)a+=1;
 		if(b%2==1)b-=1;
 		for(n=a;n<=b;n+=2)
 		{
-			//System.out.println(n+" ");
-			
-			p=0;d=0;aux=n;
-			for(i=1;i*i<=n;i++)
-			{
-				if(n/(i*i)==1 && n%(i*i)==0)
-				{p++;break;}
-			}
+			//if(n%10000000==0)System.out.println(n+" ");
+			d=0;aux=n;
 			i=1;
 //			while(n>=i && n>0)
 //			{
@@ -30,15 +24,17 @@ public class Pare {
 //			}
 			while(n>0)
 			{
-				if(n%2==1)d++;
-				n/=2;
-				
+				d+=n%2;
+				n>>=1;
+				if(d>2) break;
 			}
 			n=aux;
-			Par+=p;
 			if(d==2)Doi++;	
 		}
-		System.out.println(Par+" "+Doi);
+		a=(int) Math.sqrt(a);
+		for(i=a;i*i<=b && i*i>=a;i+=2)
+		{p++;}
+		System.out.println(p+" "+Doi);
 		
 	}
 
