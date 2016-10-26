53bb8a473e2dfd053353047e55da3f72667bdde5
Lazzer <Lazzer@Lazzer-PC>
2016-10-26 23:38:36 +0300

    Calculate the natural number "e"

---
+++ src/Dobanda_An.java
@@ -0,0 +1,14 @@
+
+public class Dobanda_An {
+
+	public static void main(String[] args) {
+		double n=21470000,y=1,i,e=1;
+		for(i=1;i<=n*y;i++)
+		{
+			e=e*(1+(1/n));
+		}
+		System.out.println(e);
+
+	}
+
+}
